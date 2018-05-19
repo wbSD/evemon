6d4e587900948a41b7dc9a7fa8a85f34e8455a70
wbSD <sd@workbook.net>
2018-05-19 17:26:01 +0200

    Add ESIMethod to generic methods enum

--- src/EVEMon.Common/Enumerations/CCPAPI/ESIAPIGenericMethods.cs
+++ src/EVEMon.Common/Enumerations/CCPAPI/ESIAPIGenericMethods.cs
@@ -17,86 +17,101 @@ namespace EVEMon.Common.Enumerations.CCPAPI
         [Header("EVE Server Status")]
         [Description("The status of the EVE server.")]
         [Update(UpdatePeriod.Minutes5, UpdatePeriod.Never, UpdatePeriod.Hours1, CacheStyle.Short)]
+        [ESIMethod("/v1/status/")]
         ServerStatus,
-        
+
         /// <summary>
         /// Used to convert IDs to Names.
         /// </summary>
+        [ESIMethod("/v2/universe/names/")]
         CharacterName,
 
         /// <summary>
         /// Used to convert Names to IDs.
         /// </summary>
         CharacterID,
-        
+
         /// <summary>
         /// Retrieves information about a conquerable station.
         /// </summary>
+        [ESIMethod("/v2/universe/stations/{0}/")]
         StationInfo,
 
         /// <summary>
         /// Retrieves information about a citadel.
         /// </summary>
+        [ESIMethod("/v1/universe/structures/{0}/", Scope = "esi-universe.read_structures.v1")]
         CitadelInfo,
 
         /// <summary>
         /// Retrieves information about a planet.
         /// </summary>
+        [ESIMethod("/v1/universe/planets/{0:D}/")]
         PlanetInfo,
 
         /// <summary>
         /// List of alliances in EVE.
         /// </summary>
+        [ESIMethod("/v1/alliances/")]
         AllianceList,
-        
+
         /// <summary>
         /// Factional warfare statistics for all EVE.
         /// </summary>
+        [ESIMethod("/v1/fw/stats/")]
         EVEFactionalWarfareStats,
 
         /// <summary>
         /// Factional warfare top 100 statistics for all EVE.
         /// </summary>
+        [ESIMethod("/v1/fw/leaderboards/")]
         EVEFactionalWarfareTopStats,
 
         /// <summary>
         /// List of factions at war.
         /// </summary>
+        [ESIMethod("/v1/fw/wars/")]
         FactionWars,
 
         /// <summary>
         /// List of references of typeIDs.
         /// </summary>
         RefTypes,
-        
+
         /// <summary>
         /// Used to convert typeIDs to typeNames.
         /// </summary>
+        [ESIMethod("/v2/universe/names/")]
         TypeName,
 
         /// <summary>
         /// List of solar systems taking part in Factional Warfare and their occupancy.
         /// </summary>
+        [ESIMethod("/v1/fw/systems/")]
         FactionalWarfareSystems,
 
         /// <summary>
         /// List of solar sytems with jump gates.
         /// </summary>
+        [ESIMethod("/v1/universe/system_jumps/")]
         Jumps,
 
         /// <summary>
         /// List of solar systems with kills within the last hour.
         /// </summary>
+        [ESIMethod("/v2/universe/system_kills/")]
         Kills,
 
         /// <summary>
         /// List of solar systems that are controlled by a faction or alliance.
         /// </summary>
+        [ESIMethod("/v1/sovereignty/structures/")]
         Sovereignty,
 
         /// <summary>
         /// An individual killmail by hash.
         /// </summary>
+        [ESIMethod("/v1/killmails/{0}/{1}/")]
         KillMail
     }
 }
