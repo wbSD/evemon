7010ca668a3ca3598a46be03e28e4abb11682a90
wbSD <sd@workbook.net>
2018-05-19 17:08:17 +0200

    New attribute for defining ESI path and required scope

---
+++ src/EVEMon.Common/Attributes/ESIMethodAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EVEMon.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class ESIMethodAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ESIMethodAttribute"/> class.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="scope"></param>
+        public ESIMethodAttribute(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; }
+
+        public string Scope { get; set; }
+    }
+}
