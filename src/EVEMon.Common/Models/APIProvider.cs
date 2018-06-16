d9d9c7677bce3ca1468f79414ff47ff1f297286c
wbSD <sd@workbook.net>
2018-06-16 23:25:16 +0200

    Added entity tag as an ESI parameter in the provider

--- src/EVEMon.Common/Models/APIProvider.cs
+++ src/EVEMon.Common/Models/APIProvider.cs
@@ -140,10 +140,11 @@ namespace EVEMon.Common.Models
         /// <param name="method"></param>
         /// <param name="callback">The callback to invoke once the query has been completed.</param>
         /// <param name="state">State to be passed to the callback when it is used.</param>
+        /// <param name="eTag">The previous result's entity tag, if applicable.</param>
         public void QueryEsiAsync<T>(Enum method, ESIRequestCallback<T> callback,
-            object state = null) where T : class
+            object state = null, string eTag = null) where T : class
         {
-            QueryEsiAsync(method, callback, state, new EsiParams());
+            QueryEsiAsync(method, callback, state, new EsiParams() { ETag = eTag });
         }
 
         /// <summary>
@@ -154,10 +155,11 @@ namespace EVEMon.Common.Models
         /// <param name="id">The ID to query.</param>
         /// <param name="callback">The callback to invoke once the query has been completed.</param>
         /// <param name="state">State to be passed to the callback when it is used.</param>
+        /// <param name="eTag">The previous result's entity tag, if applicable.</param>
         public void QueryEsiAsync<T>(Enum method, long id, ESIRequestCallback<T> callback,
-            object state = null) where T : class
+            object state = null, string eTag = null) where T : class
         {
-            QueryEsiAsync(method, callback, state, new EsiParams() { ParamOne = id });
+            QueryEsiAsync(method, callback, state, new EsiParams() { ParamOne = id, ETag = eTag });
         }
 
         /// <summary>
@@ -169,12 +171,13 @@ namespace EVEMon.Common.Models
         /// <param name="data">The string query data for the second GET parameter.</param>
         /// <param name="callback">The callback to invoke once the query has been completed.</param>
         /// <param name="state">State to be passed to the callback when it is used.</param>
+        /// <param name="eTag">The previous result's entity tag, if applicable.</param>
         public void QueryEsiAsync<T>(Enum method, long id, string data,
-            ESIRequestCallback<T> callback, object state = null) where T : class
+            ESIRequestCallback<T> callback, object state = null, string eTag = null) where T : class
         {
             QueryEsiAsync(method, callback, state, new EsiParams()
             {
-                ParamOne = id, GetData = data
+                ParamOne = id, GetData = data, ETag = eTag
             });
         }
 
@@ -186,10 +189,11 @@ namespace EVEMon.Common.Models
         /// <param name="postData">The data to submit in the POST body.</param>
         /// <param name="callback">The callback to invoke once the query has been completed.</param>
         /// <param name="state">State to be passed to the callback when it is used.</param>
+        /// <param name="eTag">The previous result's entity tag, if applicable.</param>
         public void QueryEsiAsync<T>(Enum method, string postData, ESIRequestCallback<T> callback,
-            object state = null) where T : class
+            object state = null, string eTag = null) where T : class
         {
-            QueryEsiAsync(method, callback, state, new EsiParams() { PostData = postData });
+            QueryEsiAsync(method, callback, state, new EsiParams() { PostData = postData, ETag = eTag });
         }
 
         /// <summary>
@@ -200,12 +204,13 @@ namespace EVEMon.Common.Models
         /// <param name="id">The ID to query.</param>
         /// <param name="callback">The callback to invoke once the query has been completed.</param>
         /// <param name="state">State to be passed to the callback when it is used.</param>
+        /// <param name="eTag">The previous result's entity tag, if applicable.</param>
         public void QueryEsiAsync<T>(Enum method, string token, long id, ESIRequestCallback<T> callback,
-            object state = null) where T : class
+            object state = null, string eTag = null) where T : class
         {
             QueryEsiAsync(method, callback, state, new EsiParams()
             {
-                Token = token, ParamOne = id
+                Token = token, ParamOne = id, ETag = eTag
             });
         }
 
@@ -218,12 +223,13 @@ namespace EVEMon.Common.Models
         /// <param name="id">The ID to query.</param>
         /// <param name="callback">The callback to invoke once the query has been completed.</param>
         /// <param name="state">State to be passed to the callback when it is used.</param>
+        /// <param name="eTag">The previous result's entity tag, if applicable.</param>
         public void QueryEsiAsync<T>(Enum method, string token, long character, long id,
-            ESIRequestCallback<T> callback, object state = null) where T : class
+            ESIRequestCallback<T> callback, object state = null, string eTag = null) where T : class
         {
             QueryEsiAsync(method, callback, state, new EsiParams()
             {
-                Token = token, ParamOne = character, ParamTwo = id
+                Token = token, ParamOne = character, ParamTwo = id, ETag = eTag
             });
         }
 
@@ -250,7 +256,7 @@ namespace EVEMon.Common.Models
             // Lazy download
             Uri url = GetESIUrl(method, data.ParamOne, data.ParamTwo, data.GetData);
 
-            Util.DownloadJsonAsync<T>(url, data.Token, SupportsCompressedResponse, data.PostData)
+            Util.DownloadJsonAsync<T>(url, data.Token, SupportsCompressedResponse, data.PostData, eTag: data.ETag)
                 .ContinueWith(task =>
                 {
                     var esiResult = new EsiResult<T>(task.Result);
@@ -293,6 +299,7 @@ namespace EVEMon.Common.Models
             public string GetData;
             public string PostData;
             public string Token;
+            public string ETag;
         }
 
         #endregion
